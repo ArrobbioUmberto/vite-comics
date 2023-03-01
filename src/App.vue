f69998fc8f57a517a4343a59f2b1fbd7cbf12d0d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-01 18:10:42 +0100

    FEAT import all vue files

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import ComicsHeader from './components/ComicsHeader.vue';
 import ComicsMain from './components/ComicsMain.vue';
+import ComicsFooter from './components/ComicsFooter.vue';
 </script>
 
 <template>
   <ComicsHeader></ComicsHeader>
   <ComicsMain></ComicsMain>
+  <ComicsFooter></ComicsFooter>
 </template>
 
 <style scoped>
