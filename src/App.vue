98fbc6ff4cf425966413bbe8906094fcb2052cf1
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-01 19:53:49 +0100

    DELETE old style css

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,4 @@ import ComicsFooter from './components/ComicsFooter.vue';
   <ComicsFooter></ComicsFooter>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
