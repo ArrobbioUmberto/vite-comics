631889d09b6b112c36f1ed1a588c0cb03d444a67
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-01 16:30:03 +0100

    FEAT new vue and header

---
+++ src/components/ComicsHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <header>
+        <div>
+            <img src="/img/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li>characters</li>
+            <li>comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>videos</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+        </ul>
+    </header>
+</template>
+
+<style>
+li {
+    list-style-type: none;
+}
+</style>
