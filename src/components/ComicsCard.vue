1cde6a0bd4d8cf203f9e855d204607012eb3fb99
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-02 16:57:44 +0100

    FEAT image and text

--- src/components/ComicsCard.vue
+++ src/components/ComicsCard.vue
@@ -1,19 +1,24 @@
 <script>
 export default {
     props: {
-        thumbs: String,
+        thumb: String,
         series: String,
     }
 }
 </script>
 <template>
     <li>
-        <img src="thumbs" alt="">
+        <img :src="thumb" alt="">
         <h3>{{ series }}</h3>
     </li>
 </template>
 <style lang="scss" scoped>
 h3 {
     font-size: 10px;
+}
+
+img {
+    width: 50px;
+    height: 50px;
 }
 </style>
