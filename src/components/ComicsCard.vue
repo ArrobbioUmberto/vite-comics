bc018bcd79fd9dcdfa314b58a70570308fa9c07a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-02 16:37:49 +0100

    FEAT create new vue card

---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        thumbs: String,
+        series: String,
+    }
+}
+</script>
+<template>
+    <li>
+        <img src="thumbs" alt="">
+        <h3>{{ series }}</h3>
+    </li>
+</template>
+<style lang="scss" scoped>
+h3 {
+    font-size: 10px;
+}
+</style>
